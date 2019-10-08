86e098dbf7ec2862feff961e0f2361d550b2f23a
Evgeniy <ezavalishin@gmail.com>
2019-10-09 00:31:40 +0400

    add id to user

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -23,6 +23,7 @@ class UserResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'vk_user_id' => $this->vk_user_id,
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
